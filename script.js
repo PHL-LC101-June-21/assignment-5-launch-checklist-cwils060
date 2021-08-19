d62e5835eac63b36cc99eb1157dbe41b6b7dd148
Corinne <CORINNE@Corinnes-Mac-mini.local>
2021-08-18 22:04:40 -0400

    window alert

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ window.addEventListener("load", function() {
    })
    
 });
+
+window.addEventListener("load", function(){
+    let form = document.querySelector('testForm');
+    form.addEventListener("submit", function(event){
+        let pilotName= document.querySelector('input [name=pilotName]');
+        let coPilotname= document.querySelector('input [name=copilotName]');
+        let fuelLevel = document.querySelector('input [name=fuelLevel]');
+        let cargoMass= document.querySelector('input [name=cargoMass]');
+        if(pilotName.value == "" || coPilotname.value == "" || fuelLevel.value == "" || cargoMass.value == ""){
+            window.alert ("All fields are required!");
+            event.preventDefault();
+        }
+    });
+});
+
+
